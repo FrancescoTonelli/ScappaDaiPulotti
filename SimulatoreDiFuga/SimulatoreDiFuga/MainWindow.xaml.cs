978982d5a6b73e0789e064a3efbdb82f9510d775
FrancescoTonelli <francesco.tonelli01@gmail.com>
2020-12-17 08:45:29 +0100

    Aggiunta delle immagini, implementazione movimento del truffatore

--- SimulatoreDiFuga/SimulatoreDiFuga/MainWindow.xaml.cs
+++ SimulatoreDiFuga/SimulatoreDiFuga/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace SimulatoreDiFuga
 {
@@ -20,9 +21,35 @@ namespace SimulatoreDiFuga
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly Uri uriTruffatore = new Uri("truffatore.png", UriKind.Relative);
         public MainWindow()
         {
             InitializeComponent();
+
+            Thread t1 = new Thread(new ThreadStart(MuoviTruffatore));
+
+            ImageSource immTruffatore = new BitmapImage(uriTruffatore);
+            imgTruffatore.Source = immTruffatore;
+
+            t1.Start();
+        }
+
+        public void MuoviTruffatore()
+        {
+            int margineDaSpostare = 700;
+            while (margineDaSpostare > 50)
+            {
+                margineDaSpostare -= 50;
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+
+                    imgTruffatore.Margin = new Thickness(margineDaSpostare, 270, 0, 0);
+
+                }));
+                Random r = new Random();
+                Thread.Sleep(TimeSpan.FromMilliseconds(r.Next(1, 1001)));
+            }
+
         }
     }
 }
